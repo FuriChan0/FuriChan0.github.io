fd4eb731cf64c57e685b1bf1aed53fe51a1f84df
FuriChan0 <92718389+FuriChan0@users.noreply.github.com>
2023-07-16 21:47:14 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -107,6 +107,7 @@ let usercard = document.getElementById("usercard");
 
 let p = document.createElement("p");
 
-p.innerText = '${tg.initDataUnsafe.user.first_name}\n${tg.initDataUnsafe.user.last_name}'
+p.innerText = '${tg.initDataUnsafe.user.first_name}
+${tg.initDataUnsafe.user.last_name}'
 
 usercard.appendChild(p);
