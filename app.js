15f9a7fcb4a1cda45df12c5849b2395858cede81
FuriChan0 <92718389+FuriChan0@users.noreply.github.com>
2023-07-18 02:04:45 +0300

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ btn1.addEventListener("click", function()
         tg.MainButton.setText("Выбран товар №1");
         item = "1";
         tg.MainButton.show();
-        btn1.setText("Отменить");
+        btn1.textContent("Отменить");
     }
 });
 
