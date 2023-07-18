3f15481e364ab88562f25bd5f8e409d69b2b8246
FuriChan0 <92718389+FuriChan0@users.noreply.github.com>
2023-07-18 03:07:35 +0300

    Update app.js

--- app.js
+++ app.js
@@ -9,22 +9,9 @@ let item = "";
 
 const btn = [];
 
-for (var i = 1; i <= 6; i++)
+for (let i = 1; i <= 6; i++)
 {
     btn[i] = document.getElementById('btn' + i);
-}
-
-/*
-let btn1 = document.getElementById("btn1")
-let btn2 = document.getElementById("btn2")
-let btn3 = document.getElementById("btn3")
-let btn4 = document.getElementById("btn4")
-let btn5 = document.getElementById("btn5")
-let btn6 = document.getElementById("btn6")
-*/
-/*
-for (let i = 0; i < 6; i++)
-{
     btn[i].addEventListener("click", function()
     {
         if (tg.MainButton.isVisible)
@@ -34,121 +21,10 @@ for (let i = 0; i < 6; i++)
         }
         else
         {
-            tg.MainButton.setText("Выбран товар №" + (i + 1));
-            item = i+1;
+            tg.MainButton.setText("Выбран товар №" + i);
+            item = i.toString();
             tg.MainButton.show();
             btn[i].textContent = "Отменить";
         }
     });
 }
-*/
-
-btn[i].addEventListener("click", function()
-{
-    if (tg.MainButton.isVisible)
-    {
-        tg.MainButton.hide();
-        btn[i].textContent = "Добавить";
-    }
-    else
-    {
-        tg.MainButton.setText("Выбран товар №1");
-        item = "1";
-        tg.MainButton.show();
-        btn[i].textContent = "Отменить";
-    }
-});
-
-btn[i].addEventListener("click", function()
-{
-    if (tg.MainButton.isVisible)
-    {
-        tg.MainButton.hide();
-        btn[i].textContent = "Добавить";
-    }
-    else
-    {
-        tg.MainButton.setText("Выбран товар №2");
-        item = "2";
-        tg.MainButton.show();
-        btn[i].textContent = "Отменить";
-    }
-});
-
-btn[i].addEventListener("click", function()
-{
-    if (tg.MainButton.isVisible)
-    {
-        tg.MainButton.hide();
-        btn[i].textContent = "Добавить";
-    }
-    else
-    {
-        tg.MainButton.setText("Выбран товар №3");
-        item = "3";
-        tg.MainButton.show();
-        btn[i].textContent = "Отменить";
-    }
-});
-
-btn[i].addEventListener("click", function()
-{
-    if (tg.MainButton.isVisible)
-    {
-        tg.MainButton.hide();
-        btn[i].textContent = "Добавить";
-    }
-    else
-    {
-        tg.MainButton.setText("Выбран товар №4");
-        item = "4";
-        tg.MainButton.show();
-        btn[i].textContent = "Отменить";
-    }
-});
-
-btn[i].addEventListener("click", function()
-{
-    if (tg.MainButton.isVisible)
-    {
-        tg.MainButton.hide();
-        btn[i].textContent = "Добавить";
-    }
-    else
-    {
-        tg.MainButton.setText("Выбран товар №5");
-        item = "5";
-        tg.MainButton.show();
-        btn[i].textContent = "Отменить";
-    }
-});
-
-btn[i].addEventListener("click", function()
-{
-    if (tg.MainButton.isVisible)
-    {
-        tg.MainButton.hide();
-        btn[i].textContent = "Добавить";
-    }
-    else
-    {
-        tg.MainButton.setText("Выбран товар №6");
-        item = "6";
-        tg.MainButton.show();
-        btn[i].textContent = "Отменить";
-    }
-});
-/*
-Telegram.WebApp.onEvent("mainButtonClicked", function()
-{
-    tg.sendData(item);
-});
-
-let usercard = document.getElementById("usercard");
-
-let p = document.createElement("p");
-
-p.innerText = '${tg.initDataUnsafe.user.first_name}\n${tg.initDataUnsafe.user.last_name}'
-
-usercard.appendChild(p);
-*/
