7f10cdc82fc7339a26f114ece34d5fd468e2ec64
FuriChan0 <92718389+FuriChan0@users.noreply.github.com>
2023-07-19 20:52:32 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -9,9 +9,10 @@ let item = "";
 let numProducts = 6;
 
 const buttonsContainer = document.getElementById('buttonsContainer');
+const userCard = document.getElementById('usercard');
+let quantityContainer = null;
 
-for (let i = 1; i <= numProducts; i++)
-{
+for (let i = 1; i <= numProducts; i++) {
     const button = document.createElement('button');
     button.className = 'btn';
     button.id = 'btn' + i;
@@ -31,25 +32,82 @@ for (let i = 1; i <= numProducts; i++)
     button.addEventListener('click', createClickListener(i));
 }
 
-function createClickListener(index)
-{
+function createClickListener(index) {
     return function() {
-        if (tg.MainButton.isVisible)
-        {
+        if (tg.MainButton.isVisible) {
             tg.MainButton.hide();
-            for (let j = 1; j <= numProducts; j++)
-            {
+            for (let j = 1; j <= numProducts; j++) {
                 document.getElementById('btn' + j).textContent = 'Добавить';
             }
-        }
-        else
-        {
+            clearUserCard();
+        } else {
             tg.MainButton.setText('Выбран товар №' + index);
             item = String(index);
             tg.MainButton.show();
             document.getElementById('btn' + index).textContent = 'Отменить';
+            showUserCard(index);
         }
     };
+}
+
+function showUserCard(index) {
+    clearUserCard();
+
+    if (quantityContainer === null)
+    {
+        quantityContainer = document.createElement('div');
+        quantityContainer.className = 'quantity';
+    }
+
+    const minusButton = document.createElement('button');
+    minusButton.textContent = '-';
+    minusButton.className = 'quantity-btn';
+    minusButton.addEventListener('click', decreaseQuantity);
+
+    const quantityText = document.createElement('span');
+    quantityText.textContent = 'Выбрано: 1';
+    quantityText.className = 'quantity-text';
+
+    const plusButton = document.createElement('button');
+    plusButton.textContent = '+';
+    plusButton.className = 'quantity-btn';
+    plusButton.addEventListener('click', increaseQuantity);
+
+    quantityContainer.innerHTML = ''; // Очищаем содержимое блока
+
+    quantityContainer.appendChild(minusButton);
+    quantityContainer.appendChild(quantityText);
+    quantityContainer.appendChild(plusButton);
+
+    const itemContainer = document.getElementById('btn' + index).parentNode;
+    itemContainer.style.position = 'relative'; // Добавлено
+    itemContainer.appendChild(quantityContainer);
+}
+
+function clearUserCard()
+{
+    if (quantityContainer !== null) { // Добавлено
+        quantityContainer.parentNode.removeChild(quantityContainer);
+        quantityContainer = null;
+    }
+}
+
+function decreaseQuantity()
+{
+    const quantityText = document.querySelector('.quantity-text');
+    let quantity = parseInt(quantityText.textContent.split(':')[1].trim(), 10);
+    if (quantity > 1) {
+        quantity--;
+        quantityText.textContent = 'Выбрано: ' + quantity;
+    }
+}
+
+function increaseQuantity()
+{
+    const quantityText = document.querySelector('.quantity-text');
+    let quantity = parseInt(quantityText.textContent.split(':')[1].trim(), 10);
+    quantity++;
+    quantityText.textContent = 'Выбрано: ' + quantity;
 }
 
 Telegram.WebApp.onEvent('mainButtonClicked', function()
