68269682ae6bd6780ec461c350ba31395aba7d9c
FuriChan0 <92718389+FuriChan0@users.noreply.github.com>
2023-07-19 20:14:13 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -8,36 +8,51 @@ tg.MainButton.color = "#2cab37"
 let item = "";
 let numProducts = 6;
 
-const btn = [];
+const buttonsContainer = document.getElementById('buttonsContainer');
 
-for (let i = 1; i <= numProducts; i++) {
-    btn[i] = document.getElementById('btn' + i);
-    btn[i].addEventListener("click", createClickListener(i));
+for (let i = 1; i <= numProducts; i++)
+{
+    const button = document.createElement('button');
+    button.className = 'btn';
+    button.id = 'btn' + i;
+    button.textContent = 'Добавить';
+
+    const img = document.createElement('img');
+    img.src = i + '.png';
+    img.alt = 'Burger';
+    img.className = 'img';
+
+    const itemContainer = document.createElement('div');
+    itemContainer.className = 'item';
+    itemContainer.appendChild(img);
+    itemContainer.appendChild(button);
+    buttonsContainer.appendChild(itemContainer);
+
+    button.addEventListener('click', createClickListener(i));
 }
 
 function createClickListener(index)
 {
-    return function()
-    {
+    return function() {
         if (tg.MainButton.isVisible)
         {
             tg.MainButton.hide();
             for (let j = 1; j <= numProducts; j++)
             {
-                btn[j].textContent = "Добавить";
+                document.getElementById('btn' + j).textContent = 'Добавить';
             }
         }
         else
         {
-            tg.MainButton.setText("Выбран товар №" + index);
+            tg.MainButton.setText('Выбран товар №' + index);
             item = String(index);
             tg.MainButton.show();
-            btn[index].textContent = "Отменить";
+            document.getElementById('btn' + index).textContent = 'Отменить';
         }
     };
 }
 
-Telegram.WebApp.onEvent("mainButtonClicked", function()
+Telegram.WebApp.onEvent('mainButtonClicked', function()
 {
     tg.sendData(item);
 });
