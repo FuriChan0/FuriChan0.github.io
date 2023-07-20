b6507780964b7a18ec75da2d15bd1d01d10ea0b2
FuriChan0 <92718389+FuriChan0@users.noreply.github.com>
2023-07-20 17:29:04 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,118 +1,119 @@
 let tg = window.Telegram.WebApp;
+tg.expand();
 
-tg.expand()
+tg.MainButton.textColor = "#FFFFFF";
+tg.MainButton.color = "#2cab37";
 
-tg.MainButton.textColor = "#FFFFFF"
-tg.MainButton.color = "#2cab37"
-
-let item = "";
 let numProducts = 6;
-
 const buttonsContainer = document.getElementById('buttonsContainer');
-const userCard = document.getElementById('usercard');
-let quantityContainer = null;
+const selectedItems = {};
 
-for (let i = 1; i <= numProducts; i++) {
-    const button = document.createElement('button');
-    button.className = 'btn';
-    button.id = 'btn' + i;
-    button.textContent = 'Добавить';
+for (let i = 1; i <= numProducts; i++)
+{
+  const button = document.createElement('button');
+  button.className = 'btn';
+  button.id = 'btn' + i;
+  button.textContent = 'Добавить';
 
-    const img = document.createElement('img');
-    img.src = i + '.png';
-    img.alt = 'Burger';
-    img.className = 'img';
+  const img = document.createElement('img');
+  img.src = i + '.png';
+  img.alt = 'Burger';
+  img.className = 'img';
 
-    const itemContainer = document.createElement('div');
-    itemContainer.className = 'item';
-    itemContainer.appendChild(img);
-    itemContainer.appendChild(button);
-    buttonsContainer.appendChild(itemContainer);
+  const itemContainer = document.createElement('div');
+  itemContainer.className = 'item';
+  itemContainer.appendChild(img);
+  itemContainer.appendChild(button);
+  buttonsContainer.appendChild(itemContainer);
 
-    button.addEventListener('click', createClickListener(i));
+  button.addEventListener('click', createClickListener(i));
 }
 
-function createClickListener(index) {
-    return function() {
-        if (tg.MainButton.isVisible) {
-            tg.MainButton.hide();
-            for (let j = 1; j <= numProducts; j++) {
-                document.getElementById('btn' + j).textContent = 'Добавить';
-            }
-            clearUserCard();
-        } else {
-            tg.MainButton.setText('Выбран товар №' + index);
-            item = String(index);
-            tg.MainButton.show();
-            document.getElementById('btn' + index).textContent = 'Отменить';
-            showUserCard(index);
-        }
-    };
-}
-
-function showUserCard(index) {
-    clearUserCard();
-
-    if (quantityContainer === null)
+function createClickListener(index)
+{
+  return function()
+  {
+    if (selectedItems[index])
     {
-        quantityContainer = document.createElement('div');
-        quantityContainer.className = 'quantity';
+      delete selectedItems[index];
+      document.getElementById('btn' + index).textContent = 'Добавить';
+      removeQuantityContainer(index);
     }
-
-    const minusButton = document.createElement('button');
-    minusButton.textContent = '-';
-    minusButton.className = 'quantity-btn';
-    minusButton.addEventListener('click', decreaseQuantity);
-
-    const quantityText = document.createElement('span');
-    quantityText.textContent = 'Выбрано: 1';
-    quantityText.className = 'quantity-text';
-
-    const plusButton = document.createElement('button');
-    plusButton.textContent = '+';
-    plusButton.className = 'quantity-btn';
-    plusButton.addEventListener('click', increaseQuantity);
-
-    quantityContainer.innerHTML = '';
-
-    quantityContainer.appendChild(minusButton);
-    quantityContainer.appendChild(quantityText);
-    quantityContainer.appendChild(plusButton);
-
-    const itemContainer = document.getElementById('btn' + index).parentNode;
-    itemContainer.style.position = 'relative';
-    itemContainer.appendChild(quantityContainer);
+    else
+    {
+      tg.MainButton.setText('Выбран товар №' + index);
+      selectedItems[index] = 1;
+      showQuantityContainer(index);
+      updateQuantityText(index); // Добавляем это, чтобы отображалось "Выбрано: 1" при выборе товара
+    }
+  };
 }
 
-function clearUserCard()
+function showQuantityContainer(index)
 {
-    if (quantityContainer !== null) {
-        quantityContainer.parentNode.removeChild(quantityContainer);
-        quantityContainer = null;
-    }
+  const itemContainer = document.getElementById('btn' + index).parentNode;
+  const quantityContainer = document.createElement('div');
+  quantityContainer.className = 'quantity';
+
+  const minusButton = document.createElement('button');
+  minusButton.textContent = '-';
+  minusButton.className = 'quantity-btn';
+  minusButton.addEventListener('click', () => decreaseQuantity(index));
+
+  const quantityText = document.createElement('span');
+  quantityText.textContent = 'Выбрано: ' + selectedItems[index];
+  quantityText.className = 'quantity-text';
+  quantityText.id = 'quantity-text-' + index;
+
+  const plusButton = document.createElement('button');
+  plusButton.textContent = '+';
+  plusButton.className = 'quantity-btn';
+  plusButton.addEventListener('click', () => increaseQuantity(index));
+
+  quantityContainer.appendChild(minusButton);
+  quantityContainer.appendChild(quantityText);
+  quantityContainer.appendChild(plusButton);
+  itemContainer.appendChild(quantityContainer);
 }
 
-function increaseQuantity()
+function removeQuantityContainer(index)
 {
-    const quantityText = document.querySelector('.quantity-text');
-    let quantity = parseInt(quantityText.textContent.split(':')[1].trim(), 10);
-    quantity++;
-    quantityText.textContent = 'Выбрано: ' + quantity;
+  const itemContainer = document.getElementById('btn' + index).parentNode;
+  const quantityContainer = itemContainer.querySelector('.quantity');
+  if (quantityContainer)
+  {
+    itemContainer.removeChild(quantityContainer);
+  }
 }
 
-function decreaseQuantity()
+function increaseQuantity(index)
 {
-    const quantityText = document.querySelector('.quantity-text');
-    let quantity = parseInt(quantityText.textContent.split(':')[1].trim(), 10);
-    if (quantity > 1) {
-        quantity--;
-        quantityText.textContent = 'Выбрано: ' + quantity;
-    }
+  selectedItems[index]++;
+  updateQuantityText(index);
+}
+
+function decreaseQuantity(index)
+{
+  if (selectedItems[index] > 1) {
+    selectedItems[index]--;
+    updateQuantityText(index);
+  }
+}
+
+function updateQuantityText(index)
+{
+  const quantityText = document.getElementById('quantity-text-' + index);
+  quantityText.textContent = 'Выбрано: ' + selectedItems[index];
 }
 
 Telegram.WebApp.onEvent('mainButtonClicked', function()
 {
-    const quantityText = document.querySelector('.quantity-text');
-    let quantity = parseInt(quantityText.textContent.split(':')[1].trim(), 10);
-    tg.sendData(item + " " + String(quantity));
+  let selectedItemsString = '';
+  for (const index in selectedItems)
+  {
+    selectedItemsString += index + ' ' + selectedItems[index] + ', ';
+  }
+  selectedItemsString = selectedItemsString.slice(0, -2); // Удаляем лишнюю запятую и пробел в конце строки
+  alert(selectedItemsString)
+  tg.sendData(selectedItemsString);
 });
