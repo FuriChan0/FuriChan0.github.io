ed220af1e9bc44756dcd08e3ff79cdae63b95fb0
FuriChan0 <92718389+FuriChan0@users.noreply.github.com>
2023-08-28 17:28:44 +0300

    Add files via upload

--- test/ClotShop/app.js
+++ test/ClotShop/app.js
@@ -8,7 +8,7 @@ tg.MainButton.color = "#2cab37"; // Устанавливаем цвет фона
 // Константы для получения данных из таблицы Google Sheets
 const SPREADSHEET_ID = '18fi5tz4n0ES-6Grbttv6PyCKXU5QuoSiU0EjBqC_o3A';
 const SHEET_NAME = 'Лист1';
-const RANGE = 'A:D';
+const RANGE = 'A:M';
 
 // Объект для хранения данных из Google Sheets
 const dataObj = {};
@@ -28,6 +28,22 @@ function init()
   });
 }
 
+const modal = document.getElementById("modal");
+
+document.addEventListener("click", handlerOutWard);
+modal.addEventListener("click", modalActive);
+
+function handlerOutWard() {
+  while (modal.firstChild) {
+    modal.removeChild(modal.firstChild);
+  }
+  modal.classList.remove("modal-active");
+}
+
+function modalActive(e) {
+  e.stopPropagation();
+}
+
 // Функция для получения данных из таблицы Google Sheets
 function getDataFromSheet()
 {
@@ -40,16 +56,37 @@ function getDataFromSheet()
     const num = data.length;
     if (data && data.length > 0)
     {
-      console.log('Все значения из таблицы:');
       for (let i = 1; i < num; i++) {
         const row = data[i];
         const imageUrl = row[0];
         const itemName = row[1];
         const itemPrice = row[2];
 
+        const itemAvial = row[3];
+        const itemAvialXS = row[4];
+        const itemAvialS = row[5];
+        const itemAvialM = row[6];
+        const itemAvialL = row[7];
+        const itemAvialXL = row[8];
+        const itemAvialXXL = row[9];
+        const itemAvialXXXL = row[10];
+        const itemComposition = row[11];
+        const itemDescription = row[12];
+
         dataObj[`imageUrl${i}`] = imageUrl;
         dataObj[`itemName${i}`] = itemName;
         dataObj[`itemPrice${i}`] = itemPrice;
+
+        dataObj[`itemAvial${i}`] = itemAvial;
+        dataObj[`itemAvialXS${i}`] = itemAvialXS;
+        dataObj[`itemAvialS${i}`] = itemAvialS;
+        dataObj[`itemAvialM${i}`] = itemAvialM;
+        dataObj[`itemAvialL${i}`] = itemAvialL;
+        dataObj[`itemAvialXL${i}`] = itemAvialXL;
+        dataObj[`itemAvialXXL${i}`] = itemAvialXXL;
+        dataObj[`itemAvialXXXL${i}`] = itemAvialXXXL;
+        dataObj[`itemComposition${i}`] = itemComposition;
+        dataObj[`itemDescription${i}`] = itemDescription;
       }
 
       createButtonsWithImages(num);
@@ -66,6 +103,7 @@ function getDataFromSheet()
 // Объект для хранения выбранных товаров и кнопок
 const selectedItems = {};
 const btn = [];
+const item = [];
 
 // Функция для создания кнопок с изображениями
 function createButtonsWithImages(num)
@@ -105,30 +143,56 @@ function createButtonsWithImages(num)
     itemContainer.appendChild(button);
     buttonsContainer.appendChild(itemContainer);
 
+    item[i] = img;
+    item[i].addEventListener("click", (e) => createClickItem(e, i));
     btn[i] = button;
-    btn[i].addEventListener("click", createClickListener(i));
+    btn[i].addEventListener("click", (e) => createClickListener(e, i));
   }
 }
 
 // Функция для создания обработчика клика по кнопке
-function createClickListener(index)
+function createClickListener(event, index)
 {
-  return function ()
+  event.stopPropagation(event);
+  if (selectedItems[index])
   {
-    if (selectedItems[index])
-    {
-      delete selectedItems[index];
-      btn[index].textContent = "Добавить";
-      removeQuantityContainer(index);
-    }
-    else
-    {
-      // btn[index].textContent = 'Отменить';
-      btn[index].classList.add("expanded");
-      selectedItems[index] = 1;
-      showQuantityContainer(index);
-    }
-  };
+    delete selectedItems[index];
+    btn[index].textContent = "Добавить";
+    removeQuantityContainer(index);
+  }
+  else
+  {
+    // btn[index].textContent = 'Отменить';
+    btn[index].classList.add("expanded");
+    selectedItems[index] = 1;
+    showQuantityContainer(index);
+  }
+}
+
+function createClickItem(event, index)
+{
+  event.stopPropagation(event);
+
+  modal.classList.add("modal-active");
+
+  const modalName = document.createElement("p");
+  modalName.className = "modal-name-text";
+  modalName.textContent = dataObj[`itemName${index}`];
+  modal.appendChild(modalName);
+
+  const modalDescription = document.createElement("p");
+  modalDescription.className = "modal-description-text";
+  modalDescription.innerHTML = "<b>Описание:</b><br>"
+  + dataObj[`itemDescription${index}`] + "<br><br><b>Состав:</b><br>"
+  + dataObj[`itemComposition${index}`] + "<br><br><b>Наличие:</b>"
+  + "<br> - XS: " + dataObj[`itemAvialXS${index}`]
+  + "<br> - S: " + dataObj[`itemAvialS${index}`]
+  + "<br> - M: " + dataObj[`itemAvialM${index}`]
+  + "<br> - L: " + dataObj[`itemAvialL${index}`]
+  + "<br> - XL: " + dataObj[`itemAvialXL${index}`]
+  + "<br> - XXL: " + dataObj[`itemAvialXXL${index}`]
+  + "<br> - XXXL: " + dataObj[`itemAvialXXXL${index}`];
+  modal.appendChild(modalDescription);
 }
 
 // Функция для отображения контейнера с количеством товаров
